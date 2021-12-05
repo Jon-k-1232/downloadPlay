eb0e99054d0d51f37d60bf69c861716c3ca9c5ea
Jon Kimmel <jonkimmel16@gmail.com>
2021-12-04 17:00:15 -0700

    start_of_testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import inputData from './ArrayData';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('Objects intialize to false', () => {
+	const falseArray = [false, false, false, false, false];
+	expect(inputData.map(item => item.changeColor)).toEqual(falseArray);
 });
